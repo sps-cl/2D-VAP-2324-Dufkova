d26e91cce85e8eb493f9106f957ede56bde3ad51
Dufkova_Uzivatel <Dufkova_Uzivatel@DESKTOP-C2ICF4V>
2024-04-08 18:23:11 +0200

    Visual studio

---
+++ cmd_1/cmd_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmd_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Zadejte čísla oddělená čárkou: ");
+                string a = Console.ReadLine();
+                string[] hodnoty = a.Split(',');
+                int[] cisla = new int[hodnoty.Length];
+
+                bool validInput = true;
+
+                for (int i = 0; i < hodnoty.Length; i++)
+                {
+                    if (!int.TryParse(hodnoty[i], out cisla[i]))
+                    {
+                        Console.WriteLine("Zadejte pouze celá čísla.");
+                        validInput = false;
+                        break;
+                    }
+                }
+
+                if (!validInput)
+                    continue;
+
+                Console.WriteLine("Největší číslo: " + cisla.Max());
+                Console.WriteLine("Nejmenší číslo: " + cisla.Min());
+
+                int postupka = 1;
+                int nejdelsiPostupka = 1;
+
+                for (int i = 1; i < cisla.Length; i++)
+                {
+                    if (cisla[i] > cisla[i - 1])
+                    {
+                        postupka++;
+                        if (postupka > nejdelsiPostupka)
+                            nejdelsiPostupka = postupka;
+                    }
+                    else
+                    {
+                        postupka = 1;
+                    }
+                }
+
+                Console.WriteLine("Délka nejdelšího vzestupného intervalu: " + nejdelsiPostupka);
+                Console.ReadLine();
+            }
+        }
+    }
+}
